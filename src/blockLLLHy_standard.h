02b68c32a54b4e88d458abf3714557f45e2e2437
gujigujigujihaah <60936873+yijinwang7@users.noreply.github.com>
2025-05-24 17:10:59 -0400

    Update blockLLLHy_standard.h

--- src/blockLLLHy_standard.h
+++ src/blockLLLHy_standard.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Yijin Wang
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 #ifndef BLOCKLLLHYSTANDARD_H
 #define BLOCKLLLHYSTANDARD_H
 
